98d297f155e1a458344407d8812d29cd5bbb8f80
danilal <danilkrasnoslobodcev@gmail.com>
2024-04-24 19:00:46 +0700

    add-runner-and-walls

--- game/Scripts/Main_character/character_body_2d.gd
+++ game/Scripts/Main_character/character_body_2d.gd
@@ -1,7 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 350
+const SPEED = 250
+@onready var animations = $AnimationPlayer
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+	
+		animations.play("walk" + direction)
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
@@ -16,4 +28,6 @@ func _physics_process(delta):
 		velocity.y = -SPEED
 	else:
 		velocity.y = 0
+
 	move_and_slide()
+	updateAnimation()
